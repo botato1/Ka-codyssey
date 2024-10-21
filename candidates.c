7cf08452684309ad64f53fcaf2eeecd048bc25e7
Gadonghyeun <167296172+botato1@users.noreply.github.com>
2024-10-21 16:55:48 +0900

    Update candidates.c

--- candidates.c
+++ candidates.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_CANDIDATES 6
+#define MAX_LENGTH 100
+
 int main() 
 {
     // 후보자들의 데이터를 저장할 배열
-    char candidate01[11][100], candidate02[11][100], candidate03[11][100], candidate04[11][100], candidate05[11][100], candidate06[11][100];
-    char* candidates[6] = { candidate01, candidate02, candidate03, candidate04, candidate05, candidate06 };
-
+    char candidates[MAX_CANDIDATES][11][MAX_LENGTH]; // 각 후보자의 속성 저장
     // 후보자 정보 속성 배열
     char* member_info[11] = { "성명", "생년월일", "성별", "메일", "국적", "BMI", "주 스킬", "보조 스킬", "한국어 등급", "MBTI", "소개" };
 
     int i = 0, j = 0;
 
     // 후보자 6명의 데이터를 입력받는 반복문
-    while (i < 6) 
+    while (i < MAX_CANDIDATES) 
     {
         printf("####################################\n");
         printf("     오디션 후보자 데이터 입력\n");
@@ -22,7 +23,9 @@ int main()
         for (j = 0; j < 11; j++) 
         {
             printf("%s: ", member_info[j]);
-            fgets(candidates[i] + j * 100, 100, stdin); // 각각의 속성을 배열에 저장
+            fgets(candidates[i][j], MAX_LENGTH, stdin); // 각각의 속성을 배열에 저장
+            // 개행 문자 제거
+            candidates[i][j][strcspn(candidates[i][j], "\n")] = '\0'; 
         }
         i++;
     }
@@ -31,14 +34,21 @@ int main()
     printf("####################################\n");
     printf("     오디션 후보자 데이터 조회 \n");
     printf("####################################\n");
-    for (i = 0; i < 6; i++) 
+    printf("=============================================================================================\n");
+    printf("성   명 | 생   일 | 성별 | 메   일            | 국적 | BMI  | 주스킬 | 보조스킬 | TOPIK | MBTI |\n");
+    printf("=============================================================================================\n");
+
+    for (i = 0; i < MAX_CANDIDATES; i++) 
     {
-        printf("=============================================================================================\n");
-        printf("%s | %s | %s | %s | %s | %s | %s | %s | %s | %s |\n",
-            candidates[i], candidates[i] + 100, candidates[i] + 200, candidates[i] + 300, candidates[i] + 400,
-            candidates[i] + 500, candidates[i] + 600, candidates[i] + 700, candidates[i] + 800, candidates[i] + 900);
-        printf("---------------------------------------------------------------------------------------------\n");
-        printf("%s\n", candidates[i] + 1000);  // 소개 출력
+        // 후보자 기본 정보 출력
+        printf("%-6s | %-8s | %-2s  | %-18s | %-4s | %-4s | %-5s | %-7s | %-5s | %-4s |\n", 
+            candidates[i][0], candidates[i][1], candidates[i][2], candidates[i][3], candidates[i][4],
+            candidates[i][5], candidates[i][6], candidates[i][7], candidates[i][8], candidates[i][9]);
+
+        // 구분선 출력
+        printf("        -------------------------------------------------------------------------------------\n");
+        // 소개 출력
+        printf("%s\n", candidates[i][10]);  
         printf("---------------------------------------------------------------------------------------------\n");
     }
 
